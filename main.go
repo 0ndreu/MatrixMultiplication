d053216791420f817c875bdbb455d00f8ed48d5d
0ndreu <andrey.artyushov@yandex.ru>
2019-10-07 23:58:32 +0300

    some routines added but it works incorrect

--- main.go
+++ main.go
@@ -6,19 +6,23 @@ import (
 )
 
 func main() {
-	n := 10
+	n := 2
 	matrix1 := make([][]int, n)
 	matrix2 := make([][]int, n)
-	matrix3 := make([][]int, n)
+	sum := make([][]int, n)
 	for i := 0; i < n; i++ {
 		matrix1[i] = make([]int, n)
 		matrix2[i] = make([]int, n)
-		matrix3[i] = make([]int, n)
+		sum[i] = make([]int, n)
 	}
 	fillMatrix(matrix1)
 	fillMatrix(matrix2)
+	for i := 0; i < n; i++ {
+		go multiple(matrix1, matrix2, sum, n, i)
+	}
 	fmt.Println(matrix1)
 	fmt.Println(matrix2)
+	fmt.Println(sum)
 
 }
 
@@ -28,5 +32,11 @@ func fillMatrix(matrix [][]int) {
 			matrix[i][j] = rand.Intn(100)
 			//looping over each element of array and assigning it a random variable
 		}
+	}
+}
+
+func multiple(matrix1, matrix2, sum [][]int, n int, i int) {
+	for j := 0; j < n; j++ {
+		sum[i][j] += matrix1[i][j] * matrix2[j][i]
 	}
 }
