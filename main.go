aebf9223440d61a003023f9f66f1a8644fc461e6
0ndreu <andrey.artyushov@yandex.ru>
2019-10-08 15:43:22 +0300

    Добавлен алгоритм без горутины + время. Код выглядит стремно, мне стыдно

--- main.go
+++ main.go
@@ -3,31 +3,57 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	n := 10
+	rand.Seed(time.Now().UTC().UnixNano())
+	var n int
+	fmt.Print("Введите размер квадратной матрицы ")
+	fmt.Scan(&n)
 	matrix1 := make([][]int, n)
 	matrix2 := make([][]int, n)
 	sum := make([][]int, n)
+	sumWithoutRoutine := make([][]int, n)
+
 	for i := 0; i < n; i++ {
 		matrix1[i] = make([]int, n)
 		matrix2[i] = make([]int, n)
 		sum[i] = make([]int, n)
+		sumWithoutRoutine[i] = make([]int, n)
 	}
 	fillMatrix(matrix1)
 	fillMatrix(matrix2)
+
+	t1 := time.Now()
 	for i := 0; i < n; i++ {
 		go multiple(matrix1, matrix2, sum, n, i)
 	}
-	fmt.Println(matrix1)
+	t2 := time.Now()
+	fmt.Println(int(t2.Sub(t1)/time.Microsecond), " microseconds with goroutine")
+
+	t3 := time.Now()
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			for k := 0; k < n; k++ {
+				sum[i][j] += matrix1[i][k] * matrix2[k][j]
+			}
+		}
+	}
+	t4 := time.Now()
+	fmt.Println(int(t4.Sub(t3)/time.Microsecond), " microseconds without goroutine")
+
+	// fmt.Println(matrix1)
 	// fmt.Println(matrix2)
 	// fmt.Println(sum)
-	outputMatrix(matrix1, n)
-	fmt.Print("\n\n")
-	outputMatrix(matrix2, n)
-	fmt.Print("\n\n")
-	outputMatrix(sum, n)
+	if n < 4 {
+		outputMatrix(matrix1, n)
+		fmt.Print("\n\n")
+		outputMatrix(matrix2, n)
+		fmt.Print("\n\n")
+		outputMatrix(sum, n)
+	}
+	fmt.Println("Correct")
 
 }
 
